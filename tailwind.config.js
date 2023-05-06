e3a617c08784ece45c7a6c0e2c072325b6b313bf
demotedc0der <andri_kuka.fshnstudent@unitir.edu.al>
2023-05-06 16:05:04 +0200

    sign in form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./srx/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./srx/**/*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
